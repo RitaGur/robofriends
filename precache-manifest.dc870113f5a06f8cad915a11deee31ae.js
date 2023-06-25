c2a7932d58f38d0de52a48b057fa0d06a34ad0c8
Rita <ritagurevich1996@gmail.com>
2023-06-25 15:50:12 +0300

    Updates

---
+++ precache-manifest.dc870113f5a06f8cad915a11deee31ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "940591d77fbcb7eee3f4",
+    "url": "/robofriends/static/css/main.6760535d.chunk.css"
+  },
+  {
+    "revision": "940591d77fbcb7eee3f4",
+    "url": "/robofriends/static/js/main.940591d7.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "439c5f88e4347e61eb15",
+    "url": "/robofriends/static/css/2.68386402.chunk.css"
+  },
+  {
+    "revision": "439c5f88e4347e61eb15",
+    "url": "/robofriends/static/js/2.439c5f88.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "7d791f0413600ca581654920f8ceca03",
+    "url": "/robofriends/index.html"
+  }
+];
